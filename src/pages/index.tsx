419cb2fb5af9912733ce97f83901cbfe9f2145d4
Takashiro Kazama <kazamanbou1209.pfm@gmail.com>
2021-11-13 02:23:38 +0900

    Mix Material-UI & styled-components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,30 @@
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
 
+const Wrapper = styled.section`
+  min-height: 100vh;
+  padding: 4rem;
+  background: bisque;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledButton = styled(Button)`
+  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+  border-radius: 3px;
+  border: 0;
+  color: ${(props) => (props.color ? props.color : 'white')};
+  height: 48px;
+  padding: 0 30px;
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
+`;
+
 const Home: React.FC = () => {
-  const Wrapper = styled.section`
-    min-height: 100vh;
-    padding: 4rem;
-    background: bisque;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-
-  const Heading = styled.h1`
-    font-size: 1rem;
-    color: ${(props) => (props.color ? props.color : 'inherit')};
-  `;
-
   return (
     <>
       <Wrapper>
-        <Button variant='outlined' color='inherit'>
-          <Heading color='inherit'>Hello World!</Heading>
-        </Button>
+        <StyledButton variant='outlined'>Hello World!</StyledButton>
       </Wrapper>
     </>
   );
